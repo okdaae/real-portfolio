308d57adf7a94b7afe1a3808a69912a4aa475c11
okdaae <dhrekdo5@kakao.com>
2023-01-28 03:30:25 +0900

    🐛 html-blue

--- main.js
+++ main.js
@@ -1,23 +1,4 @@
-// Make navbar transparent when it is on the top
-const navbar = document.querySelector('#navbar');
-const navbarHeight = navbar.getBoundingClientRect().height;
-document.addEventListener('scroll',()=> {
-    console.log(window.scrollY);
-    console.log(`navbarHeight: ${navbarHeight}`);
-    if(window.scrollY > navbarHeight) {
-        navbar.classList.add('navbar--dark');
-    } else{
-        navbar.classList.remove('navbar--dark')
-    }
-});
-
-//Handle scrolling when tapping on the navbar menu
-const navbarMenu = document.querySelector('.navbar__menu');
-navbarMenu.addEventListener('click', (event) =>{
-    const target = event.target;
-    const link = target.dataset.link;
-    if (link == null) {
-        return;
-    }
-    scrollIntoView(link);
-});
+window.addEventListener('scroll', function () {
+    const header = document.querySelector('.header')
+    header.classList.toggle('sticky', window.scrollY > 0)
+  })
